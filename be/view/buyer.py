76072e5b4669ab7b9df9138417e999ade1df51ce
jack <redpeanut@163.com>
2019-12-28 16:09:55 +0800

    修复订单与用户对应关系，新增已发货状态，订单自动取消，状态常量，订单筛选

--- be/view/buyer.py
+++ be/view/buyer.py
@@ -71,11 +71,24 @@ def payment():
         resp = generate_resp(INVALID_PARAMETER, '密码错误')
     else:
         money_old = user.money
-        order = Order.query.filter_by(order_id=order_id, state=0).first()
+        order = Order.query.filter_by(order_id=order_id, state=UNPAID, buyer_id=user_id).first()
         # 订单是否存在且未支付
         if order is None:
             resp = generate_resp(INVALID_PARAMETER, '订单号错误')
         else:
+            time_float = float(time.time())
+            time_int = int(time_float / 100000)
+            time_cur = time_float - float(time_int) * 100000
+            if time_cur - order.time > 10:
+                resp = generate_resp(INVALID_PARAMETER, '订单已自动取消')
+                buy = Buy.query.filter_by(order_id=order_id).all()
+                for buy_ in buy:
+                    # 删除buy中相关条目
+                    db.session.delete(buy_)
+                db.session.delete(order)
+                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': CANCELED}})
+                db.session.commit()
+                return resp
             order_amount = order.amount
             # 钱是否够
             if order_amount > money_old:
@@ -99,8 +112,8 @@ def payment():
                 # 商铺加钱
                 owner_money = owner.money
                 owner.money = owner_money + order_amount
-                order.state = 1
-                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': 1}})
+                order.state = PAID
+                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': PAID}})
                 db.session.commit()
                 resp = generate_resp(SUCCESS, "ok")
     return resp
@@ -148,7 +161,10 @@ def new_order():
                             amount = amount + book_temp.price * item['count']
                             continue
                 # 添加新订单
-                time_cur = float(time.time()) - 1577510400
+                time_float = float(time.time())
+                time_int = int(time_float / 100000)
+                time_cur = time_float - float(time_int) * 100000
+                # print(time_float, time_int, time_cur)
                 seller_id = Store.query.filter_by(store_id=store_id).first().user_id
                 new_order_ = Order(user_id, amount, time_cur, seller_id)
                 db.session.add(new_order_)
@@ -163,7 +179,7 @@ def new_order():
                     db.session.add(new_buy)
                 db.session.commit()
                 db_m.history_order.insert_one({'order_id': id_now, 'buyer': user_id, 'store': store_id, 'goods': book,
-                                               'total_amount': amount, 'state': 0})
+                                               'total_amount': amount, 'state': UNPAID})
                 resp = generate_resp_order(SUCCESS, id_now)
     return resp
 
@@ -188,34 +204,16 @@ def confirm_order():
         elif not check_password_hash(user.password, password):
             resp = generate_resp(INVALID_PARAMETER, '密码错误')
         else:
-            order = Order.query.filter_by(order_id=order_id, state=1).first()
+            order = Order.query.filter_by(order_id=order_id, state=PAID, buyer_id=user_id).first()
             # 订单是否存在且已支付
             if order is None:
                 resp = generate_resp(INVALID_PARAMETER, '订单号错误')
             else:
-                # order.state = 3
-                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': 3}})
+                # order.state = COMPLETED
+                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': COMPLETED}})
                 db.session.delete(order)
                 db.session.commit()
                 resp = generate_resp(SUCCESS, "ok")
-    return resp
-
-
-@bp.route('/his_order', methods=['POST'])
-def his_order():
-    token = request.headers.get('token')
-    de_token = jwt_decode(token)
-    if de_token is None:
-        resp = generate_resp(FAIL, "确认失败, token错误")
-    else:
-        json = request.json
-        user_id = json['user_id']
-        # 检查用户是否存在
-        user = User.query.filter_by(user_id=user_id).first()
-        if user is None:
-            resp = generate_resp(INVALID_PARAMETER, '用户名错误')
-        else:
-            resp = generate_resp_his_order(SUCCESS, dumps(db_m.history_order.find(), ensure_ascii=False))
     return resp
 
 
@@ -239,20 +237,20 @@ def cancel_order():
         elif not check_password_hash(user.password, password):
             resp = generate_resp(INVALID_PARAMETER, '密码错误')
         else:
-            order = Order.query.filter_by(order_id=order_id).first()
+            order = Order.query.filter_by(order_id=order_id, buyer_id=user_id).first()
             if order is None:
                 resp = generate_resp(INVALID_PARAMETER, '订单号错误')
             else:
-                if order.state == 0:
+                if order.state == UNPAID:
                     buy = Buy.query.filter_by(order_id=order_id).all()
                     for buy_ in buy:
                         # 删除buy中相关条目
                         db.session.delete(buy_)
-                    db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': 2}})
+                    db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': CANCELED}})
                     db.session.delete(order)
                     db.session.commit()
                     resp = generate_resp(SUCCESS, "ok")
-                elif order.state == 1:
+                elif order.state == PAID or order.state == DELIVERED:
                     # 修改用户金钱
                     money_old = user.money
                     order_amount = order.amount
@@ -272,8 +270,71 @@ def cancel_order():
                     # 商铺扣钱
                     owner_money = owner.money
                     owner.money = owner_money - order_amount
-                    db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': 2}})
+                    db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': CANCELED}})
                     db.session.delete(order)
                     db.session.commit()
                     resp = generate_resp(SUCCESS, "ok")
+    return resp
+
+
+@bp.route('/deliver', methods=['POST'])
+def deliver_order():
+    # 检查token
+    token = request.headers.get('token')
+    de_token = jwt_decode(token)
+    if de_token is None:
+        resp = generate_resp(FAIL, "确认失败, token错误")
+    else:
+        json = request.json
+        user_id = json['user_id']
+        order_id = json['order_id']
+        password = json['password']
+        # 检查用户是否存在
+        user = User.query.filter_by(user_id=user_id).first()
+        if user is None:
+            resp = generate_resp(INVALID_PARAMETER, '用户名错误')
+        # 密码是否正确
+        elif not check_password_hash(user.password, password):
+            resp = generate_resp(INVALID_PARAMETER, '密码错误')
+        else:
+            order = Order.query.filter_by(order_id=order_id, state=PAID, seller_id=user_id).first()
+            # 订单是否存在且已支付
+            if order is None:
+                resp = generate_resp(INVALID_PARAMETER, '订单号错误')
+            else:
+                order.state = DELIVERED
+                db_m.history_order.update_one({'order_id': order_id}, {"$set": {'state': DELIVERED}})
+                db.session.commit()
+                resp = generate_resp(SUCCESS, "ok")
+    return resp
+
+
+@bp.route('/his_order', methods=['POST'])
+def his_order():
+    # 检查token
+    token = request.headers.get('token')
+    de_token = jwt_decode(token)
+    if de_token is None:
+        resp = generate_resp(FAIL, "确认失败, token错误")
+    else:
+        json = request.json
+        user_id = json['user_id']
+        type = json['type']
+        context = json['context']
+        password = json['password']
+        # 检查用户是否存在
+        user = User.query.filter_by(user_id=user_id).first()
+        if user is None:
+            resp = generate_resp(INVALID_PARAMETER, '用户名错误')
+        # 密码是否正确
+        elif not check_password_hash(user.password, password):
+            resp = generate_resp(INVALID_PARAMETER, '密码错误')
+        else:
+            if type == "all":
+                resp = generate_resp_his_order(SUCCESS, dumps(db_m.history_order.find({'buyer': user_id}), ensure_ascii=False))
+            elif type == "store" or type == "total_amount" or type == "state":
+                resp = generate_resp_his_order(SUCCESS, dumps(db_m.history_order.find(
+                    {"$and": [{type: context}, {'buyer': user_id}]}), ensure_ascii=False))
+            else:
+                resp = generate_resp(INVALID_PARAMETER, '字段错误')
     return resp
